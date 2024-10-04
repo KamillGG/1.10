0ae02ff495c7c68b24197e028dcf392d58204f05
KamilGmitrzuk <123082737+KamillGG@users.noreply.github.com>
2024-10-04 11:22:55 +0200

    push push push

--- my-app/src/app/strona7/page.js
+++ my-app/src/app/strona7/page.js
@@ -1,28 +1,24 @@
 "use client"
-import { useEffect,useState } from "react"
 import Country from "@/components/CountryList"
+import { useEffect, useState } from "react"
 export default function Strona(){
-    const [data, setData] = useState(null)
+    const [count,setCount] = useState(null)
     useEffect(()=>{
         const getData = async ()=>{
             try{
                 const response = await fetch(`https://restcountries.com/v3.1/all`)
-                const jsonData = await response.json()
-                setData(jsonData)
-                console.log(jsonData)
+                const dataJson = await response.json()
+                setCount(dataJson)
             }catch(error){
                 console.log(error)
-            } finally{
-                
             }
         }
         getData()
     },[])
     return(
-        <div className="flex flex-wrap gap-2 h-screen justify-center items-center">
-            {data && data.map((item, idx)=>(
-                <Country lista={item} key={idx}/>
-            ))}
-        </div>
+        <div className="flex flex-wrap gap-2">
+            {count && count.map((item,idx)=>(
+            <Country key={idx} country={item}/>
+        ))}</div>
     )
 }
--- my-app/src/components/CountryList.js
+++ my-app/src/components/CountryList.js
@@ -1,9 +1,13 @@
-import Image from "next/image"
-export default function Country({lista}){
+import Image from "next/image";
+export default function Country({country}){
     return(
-        <div className="border">
-            <Image src={lista.flags.png} width={200} height={100} alt={lista.name.common}/>
-            <h1>{lista.name.common}</h1>
+        <div className="border-2 border-gray-700 p-2 flex flex-col items-center">
+            <div className="w-[200px] h-[100px] relative ">
+            <Image src={country.flags.png} layout="fill" objectFit="contain" className="border border-gray-700"/>
+            </div>
+        <h1 className="text-center font-semibold">{country.name.common}</h1>
+        <p>Capital: {country.capital}</p>
+        <p>Population: {country.population}</p>
         </div>
     )
 }
